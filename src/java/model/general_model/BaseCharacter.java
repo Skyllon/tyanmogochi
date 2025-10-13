d6f1dcbab7e3d6d06ce80e65dff151d99dbc395d
Skyllon <skylon.sys@gmail.com>
2025-10-14 04:14:19 +0700

    added horny level to base character

--- src/java/model/general_model/BaseCharacter.java
+++ src/java/model/general_model/BaseCharacter.java
@@ -9,6 +9,7 @@ public abstract class BaseCharacter {
   private Integer   age;
   private Double    hunger;
   private Double    energy;
+  private Double    horny;
   private Boolean   isFeedable;
   private Boolean   isPlayable;
   private MoodState mood;
@@ -21,6 +22,7 @@ public abstract class BaseCharacter {
   public void setAge(Integer age)                   { this.age          = age;          }
   public void setHunger(Double hunger)              { this.hunger       = hunger;       }
   public void setEnergy(Double energy)              { this.energy       = energy;       }
+  public void setHorny(Double horny)                { this.horny        = horny;        }
   public void setFeedable(Boolean isFeedable)       { this.isFeedable   = isFeedable;   }
   public void setPlayable(Boolean isPlayable)       { this.isPlayable   = isPlayable;   }
   public void setMood(MoodState mood)               { this.mood         = mood;         }
@@ -32,6 +34,7 @@ public abstract class BaseCharacter {
   public Integer getAge()                           { return this.age;          }
   public Double getHunger()                         { return this.hunger;       }
   public Double getEnergy()                         { return this.energy;       }
+  public Double getHorny()                          { return this.horny;        }
   public Boolean getFeedable()                      { return this.isFeedable;   }
   public Boolean getPlayable()                      { return this.isPlayable;   }
   public MoodState getMood()                        { return this.mood;         }
