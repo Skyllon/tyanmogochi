2043850132e4e0562d6fdac0168a8905eb47c19a
Skyllon <skylon.sys@gmail.com>
2025-10-14 03:34:50 +0700

    added type of anime girls

---
+++ src/java/model/general_model/states/TyanType.java
@@ -0,0 +1,14 @@
+package src.java.model.general_model.states;
+
+public enum TyanType {
+  YANDERE,
+  TSUNDERE,
+  KUUDERE,
+  DANDERE,
+  MOEKKO,
+  MEGANEKKO,
+  CATGIRL,
+  IDOL,
+  MAID,
+  OTAKU
+};
