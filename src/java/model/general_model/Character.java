7a81c7d61b5fd7a44c7b214f4a95eaa608b1fd14
Skyllon <skylon.sys@gmail.com>
2025-10-14 03:35:41 +0700

    added general class for all anime girls with general fields (without methods yet)

---
+++ src/java/model/general_model/Character.java
@@ -0,0 +1,16 @@
+package src.java.model.general_model;
+
+import src.java.model.general_model.states.*;
+
+public abstract class Character {
+  private String    name;
+  private String    surname;
+  private String    lovelyPhrase;
+
+  private Integer   age;
+  private Integer   hunger;
+  private Integer   energy;
+
+  private MoodState mood;
+  private TyanType  type;
+}
