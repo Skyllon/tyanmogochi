b4cdf62016a000de27560011009ea0b72a37fa0a
Skyllon <skylon.sys@gmail.com>
2025-10-14 21:56:31 +0700

    reduces amount of different types of tyan

--- src/java/model/general_model/states/TyanType.java
+++ src/java/model/general_model/states/TyanType.java
@@ -3,12 +3,6 @@ package src.java.model.general_model.states;
 public enum TyanType {
   YANDERE,
   TSUNDERE,
-  KUUDERE,
-  DANDERE,
-  MOEKKO,
-  MEGANEKKO,
   CATGIRL,
-  IDOL,
   MAID,
-  OTAKU
 };
