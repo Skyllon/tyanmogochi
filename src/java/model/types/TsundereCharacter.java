a6090629266f4389546716d375b523c4d86e79e9
Skyllon <skylon.sys@gmail.com>
2025-10-14 21:58:39 +0700

    renamed incorrect type to correct type of tyan

---
+++ src/java/model/types/TsundereCharacter.java
@@ -0,0 +1,57 @@
+package src.java.model.types;
+
+import src.java.model.general_model.BaseCharacter;
+import src.java.model.general_model.states.*;
+import src.java.model.interfaces.Feedable;
+
+public class TsundereCharacter
+extends BaseCharacter
+implements Feedable
+{
+  private Boolean isEmbarassed;
+
+  public TsundereCharacter(
+    String    name,
+    String    surname,
+    String    lovelyPhrase,
+    Integer   age,
+    Double    hunger,
+    Double    energy,
+    Double    horny,
+    Boolean   isFeedable,
+    Boolean   isPlayable,
+    MoodState mood,
+    TyanType  type,
+    Boolean   isEmbarassed
+  ) {
+    super(
+      name,
+      surname,
+      lovelyPhrase,
+      age,
+      hunger,
+      energy,
+      horny,
+      isFeedable,
+      isPlayable,
+      mood,
+      type
+    );
+
+    this.isEmbarassed = isEmbarassed;
+  }
+
+  public void setIsEmbarassed(Boolean isEmbarassed) { this.isEmbarassed = isEmbarassed; }
+  public Boolean getIsEmbarassed()                  { return this.isEmbarassed;         }
+
+  // TODO: later will be calculated by type of food
+  public void feed() { setHunger(20.2); }
+
+  public void sayCompliment() {
+    this.isEmbarassed = true;
+    this.horny += 5.25;
+    System.out.println(this.getLovelyPhrase());
+  }
+
+  // TODO: add more methods related with this type of girl
+}
