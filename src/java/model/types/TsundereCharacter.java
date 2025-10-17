16c7086aae4a85e80b9acfa062ccae77eeef3efa
Валерия Соболева <sobolekvl@mail.ru>
2025-10-17 21:49:58 +0700

    mood statements, play() method added

--- src/java/model/types/TsundereCharacter.java
+++ src/java/model/types/TsundereCharacter.java
@@ -45,13 +45,36 @@ implements Feedable
   public Boolean getIsEmbarassed()                  { return this.isEmbarassed;         }
 
   // TODO: later will be calculated by type of food
-  public void feed() { this.hunger -= 25.1; }
+  public void feed() { 
+    this.hunger -= 25.1; 
+    this.energy += 2.25;
+    if (this.hunger > 80 && this.energy > 80){
+      this.mood = MoodState.HAPPY;
+    } else if (this.hunger < 30 && this.energy < 40){
+      this.mood = MoodState.SAD;
+    }
+  }
 
   public void sayCompliment() {
     this.isEmbarassed = true;
     this.horny += 5.25;
+    if (this.horny >80 && this.mood == MoodState.HAPPY){
+      this.mood = MoodState.HORNY;
+    }
     System.out.println(this.getLovelyPhrase());
   }
-
+  public void play(){
+    if (isEmbarassed ==  true && (this.mood == MoodState.SAD || this.mood == MoodState.ANGRY || this.mood == MoodState.TIRED)){
+      return;
+    } else if (this.mood == MoodState.HAPPY){
+      this.mood = MoodState.PLAYFUL;
+      this.energy -= 10.;
+      this.hunger += 15.;
+    } else if (this.mood == MoodState.HORNY){
+      this.energy -= 15.;
+      this.hunger += 15.;
+    }
+  }
   // TODO: add more methods related with this type of girl
+  // play if mood = happy (?)
 }
