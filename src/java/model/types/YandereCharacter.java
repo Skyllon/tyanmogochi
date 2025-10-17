d4b3cb817c66f65071a56798ebc7e3fd557b5e02
Валерия Соболева <sobolekvl@mail.ru>
2025-10-17 21:40:33 +0700

    horny everywhere and method feed() added

--- src/java/model/types/YandereCharacter.java
+++ src/java/model/types/YandereCharacter.java
@@ -39,14 +39,19 @@ implements Playable
   public void play() {
     this.energy -= 5.;
     this.hunger += 10.5;
+    this.horny += 3.5;
   }
   public void sayCompliment() {
     this.horny += 10.5;
     System.out.printf("[NEWS]: a girl was killed by student called %s %s\n",
       this.name, this.surname);
   }
-
+  public void feed() {
+    this.hunger -= 10.5;
+    this.energy+=3.;
+    this.horny += 1.5;}
   // TODO: think about additional methods or fields that can describe this type well
   // maybe need to think about some level of violence that will be increase because
   // of others girls attention
+  
 }
