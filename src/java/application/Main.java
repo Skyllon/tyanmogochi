59db40da5033d12b1b8b22aa0de0091c94274b64
sblnsl <sobolekvl@mail.ru>
2025-11-02 23:37:49 +0700

    object array added for types

--- src/java/application/Main.java
+++ src/java/application/Main.java
@@ -9,6 +9,12 @@ import model.general_model.states.*;
 public class Main {
   public static void main(String[] args) {
     Scanner console = new Scanner(System.in);
+    
+    Object[] tyans = { TyanType.TSUNDERE, TyanType.YANDERE, TyanType.MAID, TyanType.CATGIRL };
+    System.out.println("Choose your chan (･ω<)☆\n1.Tsundere\n2.Yandere\n3.Maid\n4.Catgirl");
+    int chanChoice = console.nextInt();
+    console.nextLine();
+    TyanType selectedChan = (TyanType) tyans[chanChoice-1];
     System.out.print("Set her name: ");
     String name = console.nextLine();
     System.out.print("Set her surname: ");
@@ -16,24 +22,60 @@ public class Main {
     System.out.print("Set her age: ");
     int age = console.nextInt();
     console.nextLine();
-    TsundereCharacter tsundere = new TsundereCharacter(
-      name,
-      surname,
-      null,
-      age,
-      50.0,
-      50.0,
-      50.0,
-      true,
-      true,
-      MoodState.HAPPY,
-      TyanType.TSUNDERE,
-      false
-    );
-    tsundere.setName(name);
-    tsundere.setSurname(surname);
-    tsundere.setAge(age);
+    
+    Object chan=null;
+    try {
+      switch (chanChoice){
+        case 1:
+          TsundereCharacter tsundere = new TsundereCharacter(
+                name, surname, null, age, 50.0, 50.0, 50.0,
+                true, true, MoodState.HAPPY, TyanType.TSUNDERE, false
+            );
+            tsundere.setName(name);
+            tsundere.setSurname(surname);
+            tsundere.setAge(age);
+            chan = tsundere;
+            break;
+        case 2:
+          YandereCharacter yandere = new YandereCharacter(
+                name, surname, null, age, 50.0, 50.0, 50.0,
+                true, true, MoodState.HAPPY, TyanType.YANDERE
+            );
+            yandere.setName(name);
+            yandere.setSurname(surname);
+            yandere.setAge(age);
+            chan = yandere;
+            break;
+        case 3:
+          MaidCharacter maid = new MaidCharacter(
+                name, surname, null, age, 50.0, 50.0, 50.0,
+                true, true, MoodState.HAPPY, TyanType.MAID
+            );
+            maid.setName(name);
+            maid.setSurname(surname);
+            maid.setAge(age);
+            chan = maid;
+            break;
+        case 4:
+          CatgirlCharacter catgirl = new CatgirlCharacter(
+                name, surname, null, age, 50.0, 50.0, 50.0,
+                true, true, MoodState.HAPPY, TyanType.CATGIRL
+            );
+            catgirl.setName(name);
+            catgirl.setSurname(surname);
+            catgirl.setAge(age);
+            chan = catgirl;
+            break;
+        default:
+          throw new IllegalArgumentException("Invalid input");
+      }
+
+    } catch (NumberFormatException e){
+      System.out.println("Invalid input");
+    }
+
     while (true){
+      
     System.out.println("What do you wanna do?~\n1.Feed\n2.Play\n3.Make a compliment\n4.Exit");
     try {
       int choice = console.nextInt();
@@ -41,15 +83,22 @@ public class Main {
         case 1:
         System.out.println("Choose the type of food you wanna feed me ♡(>ᴗ•)\n1.Matcha latte\n2.Mochi\n3.Sushi\n4.Tayaki\n5.Ramen");
           int food = console.nextInt();
-          tsundere.feed();
+          console.nextLine();
+          if (chan instanceof TsundereCharacter){
+            ((TsundereCharacter) chan).feed();
+          }
           break;
         case 2:
         
-          tsundere.play();
+          if (chan instanceof TsundereCharacter){
+            ((TsundereCharacter) chan).play();
+          }
           break;
         case 3:
             String compliment = console.next();
-          tsundere.sayCompliment();
+            if (chan instanceof TsundereCharacter){
+              ((TsundereCharacter) chan).sayCompliment();
+            }
           break;
         case 4:
         console.close();
